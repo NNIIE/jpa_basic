564d657ba5f4bbad11a29882f525f4d55604ba4b
Greg.Study <greg0917@naver.com>
2022-05-10 05:07:49 +0900

    영속성 컨텍스트

--- basic/src/main/java/hellojpa/JpaMain.java
+++ basic/src/main/java/hellojpa/JpaMain.java
@@ -16,10 +16,18 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("LEE");
-            em.persist(member);
+            // 비영속
+            Member member = new Member(102L, "LEE");
+
+            // 영속
+            em.persist(member); // 1차 캐시에 저장
+
+            // member를 조회할 때 db에서 select 해오지 않고 1차 캐시에서 조회한다. (성능 향상)
+            // 마찬가지로 데이터를 처음부터 db에서 select 해서 가져올때도 처음엔 select 쿼리문이 실행되지만 2번째부턴 처음에 가져온 값이 1차 캐시 되기 때문에 캐시에서 조회한다
+            // 즉, select 쿼리문은 처음 한번만 실행된다. (JPA는 쿼리로 가져온 데이터를 항상 캐싱한다.) (동일성도 보장한다 == 비교값은 true)
+            Member findMember = em.find(Member.class, 102L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
